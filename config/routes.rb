bd13be69efb9fe6de0303037676f04539c15473e
toddfitz <utower@hotmail.com>
2016-06-29 16:28:14 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about'
+  
+  resources :articles
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
