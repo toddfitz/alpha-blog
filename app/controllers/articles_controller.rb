c8f8cc687ecb7b31a250406ac3d0b58ad530013e
toddfitz <utower@hotmail.com>
2016-07-03 01:13:40 +0000

    Delete added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,14 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
    end
    
+   def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = "The article was succesfully deleted"
+      redirect_to articles_path
+   end
+   
+   
 
    
    private
